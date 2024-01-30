fee87f279733ea5f9b90d6fb9d8dea7111314a10
Ben_Aphirat <35913424+Tamdai@users.noreply.github.com>
2024-01-30 19:46:03 +0700

    add comment3001

--- main.py
+++ main.py
@@ -177,3 +177,4 @@ if __name__ == "__main__":
     )
 
     # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, ssl_keyfile="/etc/ssl/private/privkey.pem", ssl_certfile="/etc/ssl/certs/fullchain.pem")
+#300124
